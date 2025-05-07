501e12393f1a76fc9f455ad8676213c6bb4c499d
joshuakarthik2005 <joshuakarthik2005@gmail.com>
2025-05-07 16:09:14 +0530

    inngest corrections done

--- pages/api/inngest.js
+++ pages/api/inngest.js
@@ -2,12 +2,11 @@ import { serve } from "inngest/next";
 import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
 
 // Create an API that serves zero functions
-export const { GET, POST, PUT } = serve({
-  client: inngest,
-  functions: [
-    syncUserCreation,
-    syncUserUpdation,
-    syncUserDeletion
-    
-  ],
-});
+const handler = serve({
+    client: inngest,
+    functions: [syncUserCreation, syncUserUpdation, syncUserDeletion],
+  });
+  
+  export const { GET, POST } = handler;
+  export default handler.POST; // 👈 THIS IS REQUIRED
+  
