7a8ccc9c7996d940c76991273490614c72b447cc
joshuakarthik2005 <joshuakarthik2005@gmail.com>
2025-05-07 21:44:15 +0530

    inngest corrections done

---
+++ pages/api/inngest/index.js
@@ -0,0 +1,16 @@
+import { serve } from "inngest/next";
+import {
+  inngest,
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdation,
+} from "@/config/inngest"; // works because "@/config" = "/config"
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+    syncUserCreation,
+    syncUserUpdation,
+    syncUserDeletion,
+  ],
+});
